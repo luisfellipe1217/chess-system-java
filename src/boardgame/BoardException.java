005d0c97ca2ac0bc28beb5de25bb4df19bc5f8d0
luisfellipe1217 <luis.fellipe1217@gmail.com>
2020-09-11 09:38:50 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+
+package boardgame;
+/**
+ *
+ * @author Luis Fellipe
+ */
+public class BoardException extends RuntimeException{
+    
+    public BoardException(String msg){
+        super(msg);
+    }
+    
+}

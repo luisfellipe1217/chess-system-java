d99c9d0438118e9c8c3d9721c6fa17b000cf3131
luisfellipe1217 <luis.fellipe1217@gmail.com>
2020-09-11 14:12:48 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -31,7 +31,6 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-   
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
@@ -54,7 +53,7 @@ public class UI {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print(8 - i + " ");
             for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
@@ -62,9 +61,23 @@ public class UI {
 
     }
 
-    private static void printPiece(ChessPiece piece) {
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print(8 - i + " ");
+            for (int j = 0; j < pieces.length; j++) {
+                printPiece(pieces[i][j], possibleMoves[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
+
+    private static void printPiece(ChessPiece piece, boolean backgroud) {
+        if (backgroud) {
+            System.out.print(ANSI_BLUE_BACKGROUND);
+        }
         if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
@@ -73,6 +86,5 @@ public class UI {
             }
         }
         System.out.print(" ");
-
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,12 @@ public class ChessMatch {
         return mat;
     }
     
+    public boolean[][] possibleMoves(ChessPosition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+    
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
@@ -58,8 +64,7 @@ public class ChessMatch {
         if(!board.piece(source).possibleMove(target)){
             throw new ChessException("The chosen piece can't move to target position");
         }
-    }  
-    
+    }      
     
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
